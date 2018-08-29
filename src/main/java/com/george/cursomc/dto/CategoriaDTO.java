46dd0b31be19cc7b61d53757af265be8a5f08f8d
georgemmp <machadogeorge.p@gmail.com>
2018-08-29 13:41:17 -0300

    Validação de campos em categoria

--- src/main/java/com/george/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/george/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.george.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.george.cursomc.domain.Categoria;
 
 
@@ -10,6 +14,9 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Campo nome é obrigatório")
+	@Length(min=5, max=80, message="Deve ter mínimo entre 5 a 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/george/cursomc/services/CategoriaService.java
+++ src/main/java/com/george/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.george.cursomc.domain.Categoria;
+import com.george.cursomc.dto.CategoriaDTO;
 import com.george.cursomc.repositories.CategoriaRepository;
 import com.george.cursomc.services.exceptions.DataIntegrityException;
 import com.george.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO obj) {
+		return new Categoria(obj.getId(), obj.getNome());
 	}
 }
