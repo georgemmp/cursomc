af5135c50c2b2d9dc1ea3aa58a1e46544b09e6d8
georgemmp <machadogeorge.p@gmail.com>
2018-09-11 19:31:41 -0300

    Validação email repetido de cliente

--- src/main/java/com/george/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/george/cursomc/repositories/ClienteRepository.java
@@ -1,9 +1,12 @@
 package com.george.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.george.cursomc.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
