a42b14a9d7ea4bb7e1816aabba0ab0ac5f77316f
georgemmp <machadogeorge.p@gmail.com>
2018-09-12 14:29:58 -0300

    Busca por nome e categoria

--- src/main/java/com/george/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/george/cursomc/repositories/ProdutoRepository.java
@@ -1,9 +1,18 @@
 package com.george.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.george.cursomc.domain.Categoria;
 import com.george.cursomc.domain.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
-
+	
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
