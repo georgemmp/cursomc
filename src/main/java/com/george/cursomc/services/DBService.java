4a4903081818e17a496d1dc82c65329d43a1aff0
georgemmp <machadogeorge.p@gmail.com>
2018-09-20 11:43:08 -0300

    Implementação de perfil de cliente

--- src/main/java/com/george/cursomc/services/DBService.java
+++ src/main/java/com/george/cursomc/services/DBService.java
@@ -19,6 +19,7 @@ import com.george.cursomc.domain.PagamentoComCartao;
 import com.george.cursomc.domain.Pedido;
 import com.george.cursomc.domain.Produto;
 import com.george.cursomc.domain.enums.EstadoPagamento;
+import com.george.cursomc.domain.enums.Perfil;
 import com.george.cursomc.domain.enums.TipoCliente;
 import com.george.cursomc.repositories.CategoriaRepository;
 import com.george.cursomc.repositories.CidadeRepository;
@@ -74,9 +75,11 @@ public class DBService {
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
 		Cliente cli1 = new Cliente(null, "Maria", "georgemichel41@hotmail.com", "12158749366", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli2 = new Cliente(null, "Ana", "georgemmp@unipam.edu.br.com", "66440656007", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		Endereco e1 = new Endereco(null, "Rua tal", "12", "", "Centro", "38700000", cli1, c1);
 		Endereco e2 = new Endereco(null, "Rua tal", "45", "", "Jardim America", "38700000", cli1, c2);
+		Endereco e3 = new Endereco(null, "Rua 05", "66", "", "Jardim Itália", "38700000", cli2, c3);
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
@@ -94,6 +97,7 @@ public class DBService {
 		ped2.setPagamento(pagto2);
 		
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		cli2.addPerfil(Perfil.ADMIN);
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -108,6 +112,9 @@ public class DBService {
 		cli1.getTelefones().addAll(Arrays.asList("32895641"));
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
+		cli2.getTelefones().addAll(Arrays.asList("38224569", "998654552"));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
+		
 		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
 		ped2.getItens().addAll(Arrays.asList(ip3));
 		
@@ -119,8 +126,8 @@ public class DBService {
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
