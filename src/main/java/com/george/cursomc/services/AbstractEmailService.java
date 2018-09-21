fa5f6abd656f8fbbda5e979fe3e1c0b62bfceabd
georgemmp <machadogeorge.p@gmail.com>
2018-09-21 19:23:22 -0300

    Esqueci a senha

--- src/main/java/com/george/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/george/cursomc/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.george.cursomc.domain.Cliente;
 import com.george.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService{
@@ -67,6 +68,22 @@ public abstract class AbstractEmailService implements EmailService{
 		mmh.setSentDate(new Date(System.currentTimeMillis()));
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPassword) { 
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPassword);
+		sendMail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPassword) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPassword);
+		return sm;
 	}
 
 }
